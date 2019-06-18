89cf6e2d657851a52453e6cdf336b3b2dbcf2b76
gusadolfo123 <gusadolfo123@hotmail.com>
2019-06-18 16:53:46 -0500

    Implementacion Inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { FarmsComponent } from './farms/farms.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'farms', component: FarmsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { SearchComponent } from './shared/search/search.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { IndexComponent } from './index/index.component';
+import { FarmsComponent } from './farms/farms.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, HeaderComponent, SearchComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent, SearchComponent, HeaderComponent, IndexComponent, FarmsComponent, AboutComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
