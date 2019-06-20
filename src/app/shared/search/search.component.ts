73071e96b079d4609c23f27c88b6178747e0f811
gusadolfo123 <gusadolfo123@hotmail.com>
2019-06-19 22:05:49 -0500

    Ajuste

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MatDatepickerInputEvent } from '@angular/material';
 
 @Component({
@@ -9,13 +9,10 @@ import { MatDatepickerInputEvent } from '@angular/material';
 })
 export class SearchComponent implements OnInit {
   form: FormGroup;
-  activities = [
-    { value: 1, viewValue: 'Bicicleta' },
-    { value: 1, viewValue: 'Caminata' },
-    { value: 1, viewValue: 'Otro' },
-  ];
+  options: FormGroup;
+  activities = [{ value: 1, viewValue: 'Bicicleta' }, { value: 1, viewValue: 'Caminata' }, { value: 1, viewValue: 'Otro' }];
   destinies = [{ value: 1, viewValue: 'Bogota' }, { value: 2, viewValue: 'Medellin' }, { value: 3, viewValue: 'Cali' }];
-  dataForm: any = { op1: '', op2: '', fecha: '' };
+  dataForm: any = { op1: '', op2: '', fecha1: '', fecha2: '' };
 
   date = new FormControl(new Date());
   serializedDate = new FormControl(new Date().toISOString());
@@ -26,7 +23,12 @@ export class SearchComponent implements OnInit {
     this.form = new FormGroup({
       op1: new FormControl('', Validators.required),
       op2: new FormControl('', Validators.required),
-      fecha: new FormControl('', Validators.required),
+      fecha1: new FormControl('', Validators.required),
+      fecha2: new FormControl('', Validators.required),
+    });
+    this.options = new FormBuilder().group({
+      hideRequired: false,
+      floatLabel: 'never',
     });
   }
 
@@ -35,6 +37,6 @@ export class SearchComponent implements OnInit {
   }
 
   addEvent(event: MatDatepickerInputEvent<Date>) {
-    this.dataForm.fecha = event.value;
+    this.dataForm.fecha1 = event.value;
   }
 }
