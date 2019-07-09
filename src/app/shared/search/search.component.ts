9445f82240f8b5a40bec3d8d7deaed67041d3c14
gusadolfo123 <gusadolfo123@hotmail.com>
2019-07-08 22:16:57 -0500

    Ajuste Fechas

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -8,20 +8,30 @@ import { MatDatepickerInputEvent } from '@angular/material';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  currentDate: Date = new Date();
+  startDate: Date;
+  endDate: Date;
+
+  myFilter = (d: Date): boolean => {
+    // const day = d.getDay();
+    return d >= this.currentDate;
+  };
+
+  myFilter2 = (d: Date): boolean => {
+    // const day = d.getDay();
+    return d > this.startDate;
+  };
+
   form: FormGroup;
   options: FormGroup;
-  activities = [
-    { value: 1, viewValue: 'Bicicleta' },
-    { value: 1, viewValue: 'Caminata' },
-    { value: 1, viewValue: 'Otro' },
-  ];
+  activities = [{ value: 1, viewValue: 'Bicicleta' }, { value: 1, viewValue: 'Caminata' }, { value: 1, viewValue: 'Otro' }];
   destinies = [{ value: 1, viewValue: 'Bogota' }, { value: 2, viewValue: 'Medellin' }, { value: 3, viewValue: 'Cali' }];
   dataForm: any = { op1: '', op2: '', fecha1: '', fecha2: '' };
 
   date = new FormControl(new Date());
   serializedDate = new FormControl(new Date().toISOString());
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -42,5 +52,15 @@ export class SearchComponent implements OnInit {
 
   addEvent(event: MatDatepickerInputEvent<Date>) {
     this.dataForm.fecha1 = event.value;
+  }
+
+  addEventStart(event: MatDatepickerInputEvent<Date>) {
+    console.log(event.value);
+    this.startDate = event.value;
+  }
+
+  addEventEnd(event: MatDatepickerInputEvent<Date>) {
+    console.log(event.value);
+    this.endDate = event.value;
   }
 }
