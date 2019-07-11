1d10c453786cff81a3387660c19bbc126d88af91
gusadolfo123 <gusadolfo123@hotmail.com>
2019-07-10 21:19:24 -0500

    Ajustes Datepicker

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MatDatepickerInputEvent, MatDatepicker } from '@angular/material';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-search',
@@ -27,25 +28,19 @@ export class SearchComponent implements OnInit {
 
   form: FormGroup;
   options: FormGroup;
-  activities = [
-    { value: 1, viewValue: 'Bicicleta' },
-    { value: 1, viewValue: 'Caminata' },
-    { value: 1, viewValue: 'Otro' },
-  ];
-  destinies = [{ value: 1, viewValue: 'Bogota' }, { value: 2, viewValue: 'Medellin' }, { value: 3, viewValue: 'Cali' }];
-  dataForm: any = { op1: '', op2: '', fecha1: '', fecha2: '' };
-
+  destinies = [{ value: 1, viewValue: 'Bogota' }, { value: 2, viewValue: 'Medellin' }, { value: 3, viewValue: 'Cali' }, { value: 4, viewValue: 'Amazonas' }];
   date = new FormControl(new Date());
   serializedDate = new FormControl(new Date().toISOString());
 
   constructor() {}
 
   ngOnInit() {
-    this.form = new FormGroup({
-      op1: new FormControl('', Validators.required),
-      op2: new FormControl('', Validators.required),
+    this.form = new FormGroup({      
       fecha1: new FormControl('', Validators.required),
       fecha2: new FormControl('', Validators.required),
+      huespedes_mayores: new FormControl('', Validators.required),
+      huespedes_menores: new FormControl('', Validators.required),
+      destinies: new FormControl(this.destinies, Validators.required)
     });
     this.options = new FormBuilder().group({
       hideRequired: false,
@@ -54,20 +49,24 @@ export class SearchComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.date, this.dataForm, this.form);
+    for (const key in this.form.controls) {
+      if (this.form.controls.hasOwnProperty(key)) {
+        const element =  this.form.controls[key];
+        console.log(element.value);
+      }
+    }
   }
 
   addEvent(event: MatDatepickerInputEvent<Date>) {
-    this.dataForm.fecha1 = event.value;
   }
 
   addEventStart(event: MatDatepickerInputEvent<Date>) {
     this.startDate = event.value;
     if (this.startDate >= this.endDate) {
       this.endDate.setDate(this.startDate.getDate() + 1);
-      this.form.controls['fecha2'].setValue(this.endDate);
-      this.picker2.open();
+      this.form.controls['fecha2'].setValue(this.endDate);      
     }
+    this.picker2.open();
   }
 
   addEventEnd(event: MatDatepickerInputEvent<Date>) {
