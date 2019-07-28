d08d4649043b691de5b49398a3bd7be9b8752160
gusadolfo123 <gusadolfo123@hotmail.com>
2019-07-28 16:46:17 -0500

    Implementando Redux 2

---
+++ src/app/services/farms.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Result } from '../models/Result.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FarmsService {
+  headers: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
+
+  constructor(private client: HttpClient) {}
+
+  getFarms() {
+    const urlApi = `http://localhost:4545/api/v1/farms`;
+    return this.client.get<Result>(urlApi, { headers: this.headers });
+  }
+
+  getFarmsPerPage(id: number) {
+    const urlApi = `http://localhost:4545/api/v1/farms/${id}`;
+    return this.client.get<Result>(urlApi, { headers: this.headers });
+  }
+}
