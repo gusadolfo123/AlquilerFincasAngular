801529085c459406f5569fa45bca722ccd369a39
gusadolfo123 <gusadolfo123@hotmail.com>
2019-08-17 19:19:13 -0500

    Ajuste fincas

--- src/app/models/farm.interface.ts
+++ src/app/models/farm.interface.ts
@@ -16,6 +16,7 @@ export interface Farm {
   qualification?: number;
   city?: string;
   coordinate?: Coordinate;
+  image_card?: Image;
   images?: [Image];
   prices?: {
     low_season: SeasonPrice;
